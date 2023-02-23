24ec3edef383c43a6320d93dd5c6f3a8898b7d19
Vladislav Tarasov <tarvlad@inbox.ru>
2023-02-23 13:53:38 +0700

    1

--- classFileRead.cpp
+++ classFileRead.cpp
@@ -7,6 +7,7 @@
 #include <array>
 #include <fstream>
 
+
 template <typename type> static inline
 type getValueFromClassFileBuffer(std::vector<uint8_t> &buffer, size_t &ptr) {
     auto ret = std::byteswap(*((type *)(&(buffer[ptr]))));
@@ -14,18 +15,28 @@ type getValueFromClassFileBuffer(std::vector<uint8_t> &buffer, size_t &ptr) {
     return ret;
 }
 
+
 constexpr static auto
 initResults = std::to_array<std::string_view>({
     "File not found",
     "Error while opening file",
-    "Invalid file size"
+    "Invalid file size",
+    "Not a class file"
 });
+
+
+template <typename str1, typename str2>
+static inline std::string
+createErrorString(str1 pathStr, str2 initResultStr) {
+    return std::string(pathStr) + ": " + std::string(initResultStr);
+}
+
 
 void
 ClassFile::init(std::string &pathStr) {
     auto pathFs = std::filesystem::path(pathStr);
     if (!std::filesystem::exists(pathFs)) {
-        m_result = pathStr + ": " + std::string(initResults[0]);
+        m_result = createErrorString<std::string, std::string_view>(pathStr, initResults[0]);
         return;
     }
 
@@ -34,14 +45,14 @@ ClassFile::init(std::string &pathStr) {
     {
         std::ifstream src(pathFs, std::ios::in | std::ios::binary);
         if (!src.is_open()) {
-            m_result = pathStr + ": " + std::string(initResults[1]);
+            m_result = createErrorString<std::string, std::string_view>(pathStr, initResults[1]);
             return;
         }
 
         size_t srcSz = std::filesystem::file_size(pathFs);
         buf.reserve(srcSz);
         if (srcSz > std::numeric_limits<long>::max()) {
-            m_result = pathStr + ": " + std::string(initResults[2]);
+            m_result = createErrorString<std::string, std::string_view>(pathStr, initResults[2]);
             src.close();
             return;
         }
@@ -50,4 +61,8 @@ ClassFile::init(std::string &pathStr) {
     }
 
     m_magic = getValueFromClassFileBuffer<uint32_t>(buf, bufPtr);
+    if (m_magic != 0xCAFEBABE) {
+        m_result = createErrorString<std::string, std::string_view>(pathStr, initResults[3]);
+        return;
+    }
 }
